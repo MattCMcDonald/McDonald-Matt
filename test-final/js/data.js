ae80cfd93658d8307654a22b35f89ad516c61bee
MattCMcDonald <70535793+MattCMcDonald@users.noreply.github.com>
2020-12-10 12:17:47 -0500

    bodymovin

---
+++ test-final/js/data.js
@@ -0,0 +1,8 @@
+/* Shapes */
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('svgContainer'),
+  renderer: 'svg',
+  loop: false,
+  autoplay: true,
+  path: 'images/head.json'
+});
