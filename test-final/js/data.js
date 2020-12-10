9b4c6a7549da98e09342db61740b0d204c580c2e
MattCMcDonald <70535793+MattCMcDonald@users.noreply.github.com>
2020-12-10 12:51:36 -0500

    updated bodymovin

--- test-final/js/data.js
+++ test-final/js/data.js
@@ -1,8 +1,15 @@
 /* Shapes */
-var animation = bodymovin.loadAnimation({
-  container: document.getElementById('svgContainer'),
-  renderer: 'svg',
-  loop: false,
-  autoplay: true,
-  path: 'images/head.json'
-});
+<script></script>
+var loader = document.getElementsByClassName("svgContainer");
+function loadBMAnimation(loader) {
+  var animation = bodymovin.loadAnimation({
+    renderer: 'svg',
+    container: loader,
+    loop: false,
+    autoplay: true,
+    path: 'images/data.json'
+  });
+}
+for (var i = 0; i < loader.length; i++) {
+ loadBMAnimation(loader[i]);
+);
